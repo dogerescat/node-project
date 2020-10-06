cf620ba61fddc0a0a18bc184c011d324371e63eb
大庭秀斗 <shuto.0028.0618@gmail.com>
2020-10-06 20:27:45 +0900

    login validation

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/',(req, res) => {
-    res.render('./register.ejs');
+app.get('/', (req, res) => {
+  res.render('./register.ejs', { messageNumber: 0 });
 });
 
 app.get('/login', (req, res) => {
-    res.render('./login.ejs');
+  res.render('./login.ejs');
+});
+
+app.get('/dashboard', (req, res) => {
+  if (
+    !req.query.name ||
+    !req.query.email ||
+    !req.query.password ||
+    !req.query.confirm
+  ) {
+    res.render('./register.ejs', { messageNumber: 1 });
+  } else if (req.query.password.length <= 7) {
+    res.render('./register.ejs', { messageNumber: 2 });
+  } else if (req.query.password !== req.query.confirm) {
+    res.render('./register.ejs', { messageNumber: 3 });
+  } else {
+    res.render('./dashboard.ejs', { name: req.query.name });
+  }
 });
 
 app.listen(3000);
